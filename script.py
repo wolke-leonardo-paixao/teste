173f4be9f35f76e0018c859c9e03900eac08c8a0
Leonardo <thraryion@thraryion-IdeaPad-1-15AMN7>
2024-02-07 15:26:15 -0300

    teste

--- script.py
+++ script.py
@@ -2,7 +2,21 @@ import os
 import re
 
 # Obtém o nome da branch a partir da variável de ambiente
-view_name = os.getenv("VIEW_NAME")
+branch_name = os.getenv("BRANCH_NAME")
 
-print('O nome da view é:', view_name)
-
+# Remove o prefixo 'feature/' do nome da branch
+if branch_name is not None:
+    # Remove o prefixo 'feature/' do nome da branch, se presente
+    branch_name = re.sub(r'^feature/', '', branch_name)
+    # Define o nome da view com base no nome da branch
+    view_name = ''
+    if branch_name == 'dev':
+        view_name = 'dlh-dev-brlm-qr8'
+    elif branch_name == 'prod':
+        view_name = 'dlh-prd-brlm-zcb'
+    else:
+        view_name = 'Branch desconhecida.'
+    # Imprime o nome da view
+    print('O nome da view é:', view_name)
+else:
+    print('A variável de ambiente BRANCH_NAME não está definida.')
