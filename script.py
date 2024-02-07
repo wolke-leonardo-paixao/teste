44cae6e6590943855d10dce51b0388abc31b54ec
Leonardo <thraryion@thraryion-IdeaPad-1-15AMN7>
2024-02-07 13:35:20 -0300

    teste

---
+++ script.py
@@ -0,0 +1,20 @@
+import os
+import re
+
+# Obtém o nome da branch a partir da variável de ambiente
+branch_name = os.getenv("BRANCH_NAME")
+
+# Remove o prefixo 'feature/' do nome da branch
+branch_name = re.sub(r'^feature/', '', branch_name)
+
+# Define o nome da view com base no nome da branch
+view_name = ''
+if branch_name == 'dev':
+    view_name = 'dlh-dev-brlm-qr8'
+elif branch_name == 'prod':
+    view_name = 'dlh-prd-brlm-zcb'
+else:
+    view_name = 'Branch desconhecida.'
+
+# Imprime o nome da view
+print('O nome da view é:', view_name)
