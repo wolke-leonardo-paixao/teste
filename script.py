c522e20a4a69149f2ea7d32db239d6b37150034e
Leonardo <thraryion@thraryion-IdeaPad-1-15AMN7>
2024-02-08 11:11:02 -0300

    teste

--- script.py
+++ script.py
@@ -1,25 +1,14 @@
-#!/bin/python
 import os
 import re
-import subprocess
 
 branch_name = os.getenv("BRANCH_NAME")
 
 if branch_name is not None:
-    # Remove o prefixo 'feature/' do nome da branch, se presente
     branch_name = re.sub(r'^feature/', '', branch_name)
-    # Define o nome da view com base no nome da branch
-    view_name = ''
-    if branch_name == 'refs/heads/dev':
-        view_name = 'dlh-dev-brlm-qr8'
-    elif branch_name == 'refs/heads/prod':
-        view_name = 'dlh-prd-brlm-zcb'
-    else:
-        view_name = 'Branch desconhecida.'
-
-    print('O nome da view é:', view_name)
-
-    with open('view_name.txt', 'w') as file:
-        file.write(view_name)
+    
+    temp_file = 'branch_name.txt'
+    
+    with open(temp_file, 'w') as file:
+        file.write(branch_name)
 else:
     print('A variável de ambiente BRANCH_NAME não está definida.')
