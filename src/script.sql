9dd1346d3df56cb363c822b3efa89ae7e1cb00ad
Leonardo <thraryion@thraryion-IdeaPad-1-15AMN7>
2024-02-09 16:09:34 -0300

    teste

--- src/script.sql
+++ src/script.sql
@@ -1,20 +1,12 @@
-SET @branch = '@branch_name';
-
--- Exibir o nome do branch
-SELECT CONCAT('O nome do branch é: ', @branch);
-
--- Definir o nome da view com base no branch
 SET @view_name = CASE 
-    WHEN @branch = 'dev' THEN 'dlh-dev-brlm-qr8'
-    WHEN @branch = 'prod' THEN 'dlh-prd-brlm-zcb'
+    WHEN @branch_name = 'dev' THEN 'dlh-dev-brlm-qr8'
+    WHEN @branch_name = 'prod' THEN 'dlh-prd-brlm-zcb'
     ELSE 'Branch desconhecida.'
 END;
 
--- Exibir o nome da view
 SELECT CONCAT('O nome da view é: ', @view_name);
 
--- Criar a consulta SQL dinâmica para criar a view
-SET @sql_query = CONCAT('
+SET @sql = CONCAT('
     CREATE OR REPLACE VIEW `', @view_name, '.publishedClientRelationship.dimDistrict`
     OPTIONS(
       description="Bairro é uma subdivisão da localidade. Para saber mais, acesse: https://leroypedia.leroymerlin.com.br/wiki/Bairro"
@@ -22,7 +14,6 @@ SET @sql_query = CONCAT('
     AS SELECT * FROM ', @view_name, '.semantic.dimDistrict;'
 );
 
--- Executar a consulta SQL dinâmica
 PREPARE stmt FROM @sql_query;
 EXECUTE stmt;
 DEALLOCATE PREPARE stmt;
