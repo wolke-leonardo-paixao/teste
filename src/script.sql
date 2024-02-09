4e24b87d265854a3f7fcbecd033903273fb4c51c
Leonardo <thraryion@thraryion-IdeaPad-1-15AMN7>
2024-02-09 12:20:42 -0300

    teste

--- src/script.sql
+++ src/script.sql
@@ -1,3 +1,21 @@
+DELIMITER //
+CREATE FUNCTION get_branch_name ()
+RETURNS VARCHAR(255)
+DETERMINISTIC
+BEGIN
+    DECLARE branch_name VARCHAR(255);
+    SET branch_name = TRIM(TRAILING '\n' FROM CAST(SYSTEM_USER() AS CHAR));
+    RETURN branch_name;
+END;
+//
+DELIMITER ;
+
+-- Use a função para obter o nome da branch
+SET @branch_name = get_branch_name();
+
+-- Exibir o nome da branch
+SELECT CONCAT('O nome da branch é: ', @branch_name);
+
 SET @view_name = CASE 
     WHEN '@branch_name' = 'dev' THEN 'dlh-dev-brlm-qr8'
     WHEN '@branch_name' = 'prod' THEN 'dlh-prd-brlm-zcb'
