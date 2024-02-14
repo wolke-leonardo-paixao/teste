066018303cab40414f06eaf6c455d75c5857662f
Leonardo <thraryion@thraryion-IdeaPad-1-15AMN7>
2024-02-14 12:00:46 -0300

    teste

--- src/script.sql
+++ src/script.sql
@@ -1,18 +1,9 @@
--- set branch_name
-SET @view_name = CASE 
-    WHEN '@branch_name' = 'dev' THEN 'dlh-dev-brlm-qr8'
-    WHEN '@branch_name' = 'prod' THEN 'dlh-prd-brlm-zcb'
-    ELSE 'Branch desconhecida.'
-END;
-
-SELECT CONCAT('O nome da view é: ', @view_name);
-
 SET @sql = CONCAT('
-    CREATE OR REPLACE VIEW `', @view_name, '.publishedClientRelationship.dimDistrict`
+    CREATE OR REPLACE VIEW `', 'dlh-prd-brlm-zcb', '.publishedClientRelationship.dimDistrict`
     OPTIONS(
       description="Bairro é uma subdivisão da localidade. Para saber mais, acesse: https://leroypedia.leroymerlin.com.br/wiki/Bairro"
     )
-    AS SELECT * FROM ', @view_name, '.semantic.dimDistrict;'
+    AS SELECT * FROM ', 'dlh-prd-brlm-zcb', '.semantic.dimDistrict;'
 );
 
 PREPARE stmt FROM @sql_query;
