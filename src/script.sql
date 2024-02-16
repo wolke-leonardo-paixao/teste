c669744b8660819bb3601d9c11d253da4f3ee1d0
Leonardo <thraryion@thraryion-IdeaPad-1-15AMN7>
2024-02-16 14:16:24 -0300

    alterandoautomatica id do projeto

--- src/script.sql
+++ src/script.sql
@@ -1,9 +1,9 @@
 SET @sql = CONCAT('
-    CREATE OR REPLACE VIEW `', 'dlh-prd-brlm-zcb', '.publishedClientRelationship.dimDistrict`
+    CREATE OR REPLACE VIEW `', 'dlh-dev-brlm-qr8', '.publishedClientRelationship.dimDistrict`
     OPTIONS(
       description="Bairro é uma subdivisão da localidade. Para saber mais, acesse: https://leroypedia.leroymerlin.com.br/wiki/Bairro"
     )
-    AS SELECT * FROM ', 'dlh-prd-brlm-zcb', '.semantic.dimDistrict;'
+    AS SELECT * FROM ', 'dlh-dev-brlm-qr8', '.semantic.dimDistrict;'
 );
 
 PREPARE stmt FROM @sql;
